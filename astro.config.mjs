260633becec23c0da48aa450638aac5628ccbf73
Jacob DeJean <jacobdejean@lab-eon.local>
2023-11-06 23:28:31 -0600

    adjusting urls for live

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jacobdejean.github.io',
-    base: '/chrisdenais.com',
-});
+	site: 'https://chrisdenais.com',
+	base: '/'
+})
