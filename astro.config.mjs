999deb46b99baa678f5d465b9847d230a55798b3
Jacob DeJean <jacobdejean@lab-eon.local>
2023-11-13 19:39:52 -0600

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://chrisdenais.com',
-	base: '/'
+	site: 'https://jacobdejean.github.io',
+	base: '/chrisdenais.com'
 })
