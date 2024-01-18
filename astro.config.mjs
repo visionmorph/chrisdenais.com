135ef85d23b061835e9e558d672ffd3fca91fcd9
Jacob DeJean <jacobdejean@lab-eon.local>
2024-01-18 17:54:53 -0600

    modified .prettierrc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jacobdejean.github.io',
-	base: '/chrisdenais.com'
+  site: 'https://jacobdejean.github.io',
+  base: '/chrisdenais.com'
 })
