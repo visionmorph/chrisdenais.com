5960821f7f9bda78721b44b4fd0df281c9354a8c
Jacob DeJean <jacob@dejean.dev>
2024-04-11 22:31:07 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   // TODO: remove this
-	site: 'chrisdenais-com.pages.dev',
+	site: 'https://chrisdenais-com.pages.dev',
 	base: '/'
 })
