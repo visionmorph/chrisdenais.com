81628b17b7847db6de850320b8eb11c5e3d170e8
Christopher Denais <133643043+chrisdenais@users.noreply.github.com>
2025-11-19 07:55:27 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   // TODO: remove this
-	site: 'https://chrisdenais-com.pages.dev',
+	site: 'https://chrisdenais.com',
 	base: '/'
+	output: 'static'
 })
